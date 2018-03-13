d212854494926bf810c8898382d4e4edeea54345
SparksCode <overeck@gmail.com>
2018-03-12 20:22:41 -0500

    Remove overflow menu and minor optimizations.

--- app/src/main/java/tdkdesigns/hundredthieves/PlayerList.java
+++ app/src/main/java/tdkdesigns/hundredthieves/PlayerList.java
@@ -122,11 +122,6 @@ public class PlayerList extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
--- app/src/main/java/tdkdesigns/hundredthieves/ScheduleList.java
+++ app/src/main/java/tdkdesigns/hundredthieves/ScheduleList.java
@@ -151,11 +151,6 @@ public class ScheduleList extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
